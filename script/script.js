39f16e75b95d24a750c5ee0a9addb4a85373f618
Pavel Zhukov <zhukovpavel58@mail.ru>
2021-02-06 22:54:53 +0300

    feat: game random number | Script\script.js

---
+++ script/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function isNumber(number) {
+    return !isNaN(parseFloat(number)) && isFinite(number);
+}
+
+function game() {
+    let randomNumber = Math.ceil(Math.random() * 100) + 1;
+    
+    function gameRandomNumber() {
+        const answer = +prompt('Угадай число от 1 до 100');
+
+        if (answer === null) {
+            alert('Игра окончена');
+            return;
+        } else if (randomNumber < answer) {
+            alert('Загаданное число больше');
+            gameRandomNumber();
+        } else if (randomNumber > answer) {
+            alert('Загаданное число меньше');
+            gameRandomNumber();
+        } else if (answer === randomNumber) {
+            return alert('Поздравляю, Вы угадали!!!');
+        } else if (!isNumber(answer) || answer > 100 || answer < 0) {
+            alert('Введите число!');
+            gameRandomNumber();
+        } else {
+            alert('Что-то пошло не так');
+            return;
+        }
+    }
+    return gameRandomNumber;
+}
+const userGame = game();
+userGame();
+
+
+
